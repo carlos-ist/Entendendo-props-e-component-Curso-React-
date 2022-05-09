721684ff82fcb5589902db909dd09c481343b6ef
jose-ist <carlos.work@outlook.com>
2022-05-09 11:52:43 -0300

    Trabalhando com States (aula-34)

--- src/App.js
+++ src/App.js
@@ -1,58 +1,47 @@
 import React, {Component} from 'react';
 
-class Equipe extends Component{
+class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            nome: 'Matheus',
+            contador: 0
+        }
+
+        this.aumentar = this.aumentar.bind(this);
+        this.diminuir = this.diminuir.bind(this);
+    }
+
+    aumentar(){
+        let state = this.state;
+        state.contador += 1;
+        state.nome="Jose";
+        this.setState(state)
+    }
+
+    diminuir(){
+        let state = this.state;
+        if(state.contador === 0){
+            alert('Opa chegou a zero!');
+            return;
+        }
+        state.contador -= 1;
+        this.setState(state)
+    }
+
     render(){
         return(
             <div>
-                <Sobre
-                    name={this.props.nome}
-                    age={this.props.idade}
-                    profession={this.props.cargo}
-                />
-                <hr/>
-            </div>
-        );
-    }
-}
-
-class Sobre extends Component{
-    render(){
-        return(
-           
-           <div>
-                <h2> Olá sou o {this.props.name} </h2>
-                <h3> Cargo: {this.props.profession}</h3>
-                <h3> Idade: {this.props.age} anos </h3>
-            
+                <h1>Contador:</h1>
+                {this.state.nome}
+                <h3><button onClick={this.diminuir}> - </button>
+                    { this.state.contador }
+                <button onClick={this.aumentar}> + </button></h3>
             </div>
 
         );
     }
-}
-
-
-function App(){
-    return(
-        <div>
-
-            <h1>Conheça nossa equipe:</h1>
-            <hr/>
-
-            <Equipe 
-                nome="Matheus" 
-                cargo="Programador"
-                idade="24"
-            />
-
-            <Equipe 
-                nome="Lucas" 
-                cargo="Designer"
-                idade="15"
-            />
-
-        </div>
-
-    )
 }
 
 export default App;
