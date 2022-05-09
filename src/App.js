653d49fe97aa030250dadc7a66f79ee86a4aabd2
jose-ist <carlos.work@outlook.com>
2022-05-09 18:25:44 -0300

    Renderização Condicional - Operador Ternário (aula-39)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React, {Component} from 'react';
-import Membro from './components/Membro'
 
 class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            status: false
+        };
+        this.sair=this.sair.bind(this);
+        this.entrar=this.entrar.bind(this);
+
+    }
+
+    sair(){
+        this.setState({status:false});
+        console.log("Status: False");
+    }
+
+    entrar(){
+        this.setState({status:true});
+        console.log("Status: True");
+    }
 
     render(){
         return(
             <div>
-                <Membro nome="Visitante"/>
+                {this.state.status ?
+                <div>
+                    <h2> Bem-Vindo ao Sistema</h2>
+                    <button onClick={this.sair}>Sair no Sistema</button>
+                </div>:
+                <div>
+                    <h2>Olá visitante, faça o login!</h2>
+                    <button onClick={this.entrar}>Entrar no Sistema</button>
+                </div>
+                }
+
             </div>
         );
     }
