655986622dd1e05a053f4524bd0e8358e81134a4
jose-ist <carlos.work@outlook.com>
2022-05-10 13:58:35 -0300

    Trabalhando com Listas (aula-40)

--- src/App.js
+++ src/App.js
@@ -1,41 +1,28 @@
 import React, {Component} from 'react';
+import Feed from './components/Feed';
 
 class App extends Component{
 
     constructor(props){
         super(props);
         this.state = {
-            status: false
+            feed:[
+                {id:1, username: "Matheus", curtidas: 10, comentarios:2},
+                {id:2, username: "Lucas", curtidas: 120, comentarios:24},
+                {id:3, username: "Amanda", curtidas: 30, comentarios:12},
+                {id:4, username: "Ricardo", curtidas: 1, comentarios:0}
+            ]
         };
-        this.sair=this.sair.bind(this);
-        this.entrar=this.entrar.bind(this);
-
-    }
-
-    sair(){
-        this.setState({status:false});
-        console.log("Status: False");
-    }
-
-    entrar(){
-        this.setState({status:true});
-        console.log("Status: True");
     }
 
     render(){
         return(
             <div>
-                {this.state.status ?
-                <div>
-                    <h2> Bem-Vindo ao Sistema</h2>
-                    <button onClick={this.sair}>Sair no Sistema</button>
-                </div>:
-                <div>
-                    <h2>Olá visitante, faça o login!</h2>
-                    <button onClick={this.entrar}>Entrar no Sistema</button>
-                </div>
-                }
-
+                {this.state.feed.map((item)=>{
+                    return(
+                        <Feed id={item.id} username={item.username} curtidas={item.curtidas} comentarios={item.comentarios}/>
+                    );
+                })}
             </div>
         );
     }
